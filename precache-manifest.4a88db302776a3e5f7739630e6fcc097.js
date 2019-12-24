2f829ea4ea4575a87c999897830f617939a88493
Aaron Johnson <aaron@aaronjohnson.me>
2019-12-23 22:24:47 -0500

    Updates

---
+++ precache-manifest.4a88db302776a3e5f7739630e6fcc097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c99a48d07b26708b948be3ca17ccad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9511da1c3bffad49aed6",
+    "url": "/static/css/main.701c8b99.chunk.css"
+  },
+  {
+    "revision": "4c0ec1c1ff0abeb95be8",
+    "url": "/static/js/2.0cd2b230.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.0cd2b230.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9511da1c3bffad49aed6",
+    "url": "/static/js/main.d52cc44d.chunk.js"
+  },
+  {
+    "revision": "cb4a67b6f70dbd0c146d",
+    "url": "/static/js/runtime-main.2291ba0c.js"
+  }
+]);
